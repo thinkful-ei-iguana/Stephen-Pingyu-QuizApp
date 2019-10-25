749ff1461f8e75d4a3cac60efc772d7a6bbab7b3
john94 <ethanok0802@gmail.com>
2019-10-25 14:03:36 -0700

    the html of the questions

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 'use strict';
 
 function renderStart() {
-    //Renders first screen when users loads the page
+    //Renders first screen when users loads the page 
 }
 
 function renderQuestion() {
-    //Renders each of the questions based on the info stored in the Questions array
+    //Renders each of the questions based on the info stored in the Questions array\
+    const questionsHtml = $(`
+    <section>
+      <form id='js-questions' class='questions-form'>
+      <fieldset>
+        <div class = 'question'>
+          <legend>${questions.question}</legend>
+        </div>
+
+        <div class='options'>
+          <div class='js-options'>
+          </div>
+        </div>
+
+        <div class='q-submit'>
+          <button type='submit' id='answer'>Submit</button>
+          <button type='button' id='next'>Next</button>
+        </div>
+        </fieldset>
+      </form>
+    </section>`);
 }
 
 function renderCorrectAnswer() {
@@ -18,4 +38,5 @@ function renderWrongAnswer() {
 
 function renderFinal() {
     //Renders the last screen displaying the users score and displays a message dependant on how well they.
+
 }
