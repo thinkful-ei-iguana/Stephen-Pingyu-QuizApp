e95a023541639e0a2384d4c5578b8a11f94f3965
stephenschab <sschab689@gmail.com>
2019-10-25 15:06:32 -0600

    finished render correct answer function

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 'use strict';
 
 function renderStart() {
-    //Renders first screen when users loads the page
+  //Renders first screen when users loads the page
+    
 }
 
 function renderQuestion() {
-    //Renders each of the questions based on the info stored in the Questions array
+  //Renders each of the questions based on the info stored in the Questions array
 }
 
 function renderCorrectAnswer() {
-    //Renders the display when the user gets the answer correct
+  //Renders the page when an answer is correct
+  $('.answer').html(
+    `<h4 class="wrong-correct-title">YOU GOT IT!!!</h4>
+    <p class="wrong-correct-text>You must have you're head in the stars!</p>
+    <button type="submit" class="next-question>Next Question</button>`
+  )
 }
 
 function renderWrongAnswer() {
-    //Renders the display when the user answers incorrectly. Displaying the correct answer.
+  //Renders the display when the user answers incorrectly. Displaying the correct answer.
 }
 
 function renderFinal() {
-    //Renders the last screen displaying the users score and displays a message dependant on how well they.
+  //Renders the last screen displaying the users score and displays a message dependant on how well they.
+}
+
+function handleQuizApp(){
+  quizStart();
+  handleQuiz();
+  handleSelectOption();
+  restartQuiz
+
 }
