2c7954aa36ab39252467e81a287314301435b16a
john94 <ethanok0802@gmail.com>
2019-10-25 13:28:59 -0700

    some minor changes

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-'use strict'
+'use strict';
 
-function renderStart(){
-  //Renders first screen when users loads the page
+function renderStart() {
+    //Renders first screen when users loads the page
 }
 
-function renderQuestion(){
-  //Renders each of the questions based on the info stored in the Questions array
+function renderQuestion() {
+    //Renders each of the questions based on the info stored in the Questions array
 }
 
-function renderCorrectAnswer(){
-  //Renders the display when the user gets the answer correct
+function renderCorrectAnswer() {
+    //Renders the display when the user gets the answer correct
 }
 
-function renderWrongAnswer(){
-  //Renders the display when the user answers incorrectly. Displaying the correct answer.
+function renderWrongAnswer() {
+    //Renders the display when the user answers incorrectly. Displaying the correct answer.
 }
 
-function renderFinal(){
-  //Renders the last screen displaying the users score and displays a message dependant on how well they.
+function renderFinal() {
+    //Renders the last screen displaying the users score and displays a message dependant on how well they.
 }
