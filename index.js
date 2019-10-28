e9936d525abe90ef3d40f6e6df2ec176aadfa475
stephenschab <sschab689@gmail.com>
2019-10-28 09:55:44 -0600

    added render question funcion

--- index.js
+++ index.js
@@ -1,58 +1,43 @@
 'use strict';
 
 function startQuiz() {
-    $('.start-button').on('submit', event => {
-        event.preventDefault();
-        $('.quiz-window').html(renderQuestion());
-    });
+  $('.start-button').on('submit', event => {
+    event.preventDefault();
+    $('.quiz-window').html(renderQuestion());
+  });
 
 }
 
 function renderQuestion() {
-    //Renders each of the questions based on the info stored in the Questions array\
-    const questionsHtml = $(`
-    <section>
-      <form id='js-questions' class='questions-form'>
-      <fieldset>
-        <div class = 'question'>
-          <legend>${questions.question}</legend>
-        </div>
-
-        <div class='options'>
-          <div class='js-options'>
-          </div>
-        </div>
-
-        <div class='q-submit'>
-          <button type='submit' id='answer'>Submit</button>
-          <button type='button' id='next'>Next</button>
-        </div>
-        </fieldset>
-      </form>
-    </section>`);
-}
+  //Renders each of the questions based on the info stored in the Questions array
+  if (STORE.currQuestion < STORE.questions.length) {
+    return generateQuestion(currQuestion); 
+  }
+  else {
+    return renderFinal();
+  }
 
 function renderCorrectAnswer() {
-    //Renders the page when an answer is correct
-    $('.answer').html(
-        `<h4 class="wrong-correct-title">YOU GOT IT!!!</h4>
+  //Renders the page when an answer is correct
+  $('.answer').html(
+    `<h4 class="wrong-correct-title">YOU GOT IT!!!</h4>
     <p class="wrong-correct-text>You must have you're head in the stars!</p>
     <button type="submit" class="next-question>Next Question</button>`
-    )
+  )
 }
 
 function renderWrongAnswer() {
-    //Renders the display when the user answers incorrectly. Displaying the correct answer.
+  //Renders the display when the user answers incorrectly. Displaying the correct answer.
 }
 
 function renderFinal() {
-    //Renders the last screen displaying the users score and displays a message dependant on how well they.
+  //Renders the last screen displaying the users score and displays a message dependant on how well they.
 }
 
 function handleQuizApp() {
-    quizStart();
-    handleQuiz();
-    handleSelectOption();
-    restartQuiz()
+  quizStart();
+  handleQuiz();
+  handleSelectOption();
+  restartQuiz()
 }
 $(handleQuizApp());
