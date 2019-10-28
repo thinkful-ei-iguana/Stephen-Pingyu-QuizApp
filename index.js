41e87922ac90cc3b00d00523c4004c43b4e85f56
john94 <ethanok0802@gmail.com>
2019-10-28 13:47:51 -0700

    styling choices for the questions

--- index.js
+++ index.js
@@ -1,53 +1,59 @@
 'use strict';
 
 function trackCurrQuestion() {
-  STORE.currQuestion++;
+    STORE.currQuestion++;
 }
 
 function trackUserScore() {
-  STORE.userScore++;
+    STORE.userScore++;
 }
 
 function loadPage() {
-  $('.quiz-window').html(
-    `<img src="images/chewy-naps.jpg" alt="chewy naps with porgs" class="images"></img>
+    $('.quiz-window').html(
+        `<img src="images/chewy-naps.jpg" alt="chewy naps with porgs" class="images"></img>
       <br>
       <button type="button" id="start">Start Quiz</button>`
-  );
+    );
 }
 
 function nextQuestion() {
-  $('#start').on('click', event => {
-    event.preventDefault();
-    trackCurrQuestion();
-    $('.quiz-window').html(renderQuestion()); 
-  });
+    $('#start').on('click', event => {
+        event.preventDefault();
+        trackCurrQuestion();
+        renderQuestion();
+    });
 }
 
 function renderQuestion() {
-  let question = STORE.questions[STORE.currQuestion];
-  console.log(question, STORE.currQuestion);
-  let answerList = question.answers;
-  let renderAnswers = '';
-  answerList.forEach(item => renderAnswers +=
-    `<div>
-    <input type="radio" name="question id="${item}" val="${item}"></input>
-    <label for="${item}">${item}</label>
-    </div>`);
-  $('.quiz-window').html(
-    `<p class="question-tracker">Question: ${STORE.currQuestion} out of ${STORE.questions.length}</p>
-    <p class="score-tracker">Score: ${STORE.userScore} out of ${STORE.questions.length}</p>
+    let question = STORE.questions[STORE.currQuestion];
+    console.log(question, STORE.currQuestion);
+    let answerList = question.answers;
+    let renderAnswers = '';
+    answerList.forEach(item => renderAnswers +=
+        `
+        <li>
+        <label for="${item}">
+    <input type="radio" name="question id="${item}" val="${item}">${item}</label>
+    </li>
+    `);
+    $('.quiz-window').html(
+        `<span class="question-tracker">Question: ${STORE.currQuestion} out of ${STORE.questions.length}</span>
+    <span class="score-tracker">Score: ${STORE.userScore} out of ${STORE.questions.length}</span>
     <h3 class="question">${question.question}</h3>
     <form>
+    <div class="options">
+    <ul>
     ${renderAnswers}
-    <button class="submit-answer"><span>Submit</span></button>
+    </ul>
+    </div>
+    <button class="submit-answer button"><span>Submit</span></button>
     </form>`
-  );
+    );
 }
 
 function handleQuizApp() {
-  loadPage();
-  nextQuestion();
+    loadPage();
+    nextQuestion();
 }
 
 $(handleQuizApp);
