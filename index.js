ad902f91a8e770e4c8265a4ea2ddcc0912acb030
stephenschab <sschab689@gmail.com>
2019-10-28 14:10:38 -0600

    got question render function working

--- index.js
+++ index.js
@@ -1,35 +1,53 @@
 'use strict';
 
 function trackCurrQuestion() {
-    STORE.currQuestion++;
+  STORE.currQuestion++;
 }
 
 function trackUserScore() {
-    STORE.userScore++;
+  STORE.userScore++;
 }
 
 function loadPage() {
   $('.quiz-window').html(
-    `<img src="images/chewy-naps.jpg" alt="chewy naps" class="images"></img>
+    `<img src="images/chewy-naps.jpg" alt="chewy naps with porgs" class="images"></img>
       <br>
       <button type="button" id="start">Start Quiz</button>`
   );
 }
 
-function startQuiz() {
-    $('#start').on('click', event => {
-        event.preventDefault();
-        return $('.quiz-window').html(renderQuestion());
-    });
+function nextQuestion() {
+  $('#start').on('click', event => {
+    event.preventDefault();
+    trackCurrQuestion();
+    $('.quiz-window').html(renderQuestion()); 
+  });
 }
 
 function renderQuestion() {
-
+  let question = STORE.questions[STORE.currQuestion];
+  console.log(question, STORE.currQuestion);
+  let answerList = question.answers;
+  let renderAnswers = '';
+  answerList.forEach(item => renderAnswers +=
+    `<div>
+    <input type="radio" name="question id="${item}" val="${item}"></input>
+    <label for="${item}">${item}</label>
+    </div>`);
+  $('.quiz-window').html(
+    `<p class="question-tracker">Question: ${STORE.currQuestion} out of ${STORE.questions.length}</p>
+    <p class="score-tracker">Score: ${STORE.userScore} out of ${STORE.questions.length}</p>
+    <h3 class="question">${question.question}</h3>
+    <form>
+    ${renderAnswers}
+    <button class="submit-answer"><span>Submit</span></button>
+    </form>`
+  );
 }
 
 function handleQuizApp() {
-    loadPage();
-    startQuiz();
+  loadPage();
+  nextQuestion();
 }
 
 $(handleQuizApp);
